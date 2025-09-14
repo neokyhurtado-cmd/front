48155a69d070b5a247200a06dca6723d5838073a
Panorama Ingeniería <admin@panoramaingenieria.com>
2025-09-14 10:34:39 -0500

    fix(safe): safelist xl:grid-cols-4 [step: D3]

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./resources/**/*.vue",
     "./vendor/filament/**/*.blade.php",
   ],
+  darkMode: "class", // ← importante
+  safelist: [
+    "rounded-[36px]","aspect-[16/9]","max-w-7xl","md:grid-cols-12",
+    "md:col-span-3","md:col-span-6","lg:col-span-2","lg:col-span-8",
+    "sm:grid-cols-2","lg:grid-cols-3","xl:grid-cols-4","md:gap-8","line-clamp-3",
+    "news-featured","news-h","h-wrap","h-media","h-body","h-title","h-excerpt","h-meta"
+  ],
   theme: {
     extend: {
       fontFamily: {
